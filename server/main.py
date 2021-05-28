9b6cb6a38fad1f234eafff6c22b8ea57a37011b8
Tom Sugden <tom@tomsugden.uk>
2021-05-28 17:38:46 +0100

    First test route added

--- server/main.py
+++ server/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from enum import Enum
+
+
+class ModelName(str, Enum):
+    tom = "tom"
+    jack = "jack"
+    asia = "asia"
+
 
 app = FastAPI()
 
 
-@app.get('/')
-async def root():
-    return {"message": "Hello, Tom"}
+@app.get('/models/{model_name}')
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.tom:
+        return {"success": True, "model_name": model_name, "message": "Tom test"}
+
+    if model_name == ModelName.asia:
+        return {"success": True, "model_name": model_name, "message": "Asia test"}
+
+    return {"success": True, "model_name": model_name, "message": "Unknown test"}
